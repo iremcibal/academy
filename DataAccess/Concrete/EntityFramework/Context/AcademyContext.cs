285e50e45eb8952a5a624611b03329c2cd76fb32
Asus <Asus@DESKTOP-6HHUABE>
2023-01-11 17:16:02 +0300

    add entity framework dal's

---
+++ DataAccess/Concrete/EntityFramework/Context/AcademyContext.cs
@@ -0,0 +1,32 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class AcademyContext : DbContext
+    {
+        public DbSet<Applicant> Applicants { get; set; }
+        public DbSet<Application> Applications { get; set; }
+        public DbSet<Blacklist> Blacklists { get; set; }
+        public DbSet<Bootcamp> Bootcamps { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<State> States { get; set; }
+        public DbSet<User> Users { get;set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-6HHUABE\MSSQLSERVER01;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { 
+
+        }
+    }
+}
