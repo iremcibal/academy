7a0b586e9e3cfd44e3011b9676d48a7e329b007e
Asus <Asus@DESKTOP-6HHUABE>
2023-02-13 15:06:02 +0300

    added cors settings

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddDependencyResolvers(new ICoreModule[]
     new BusinessCoreModule()
 });
 builder.Services.AddControllers();
+
+builder.Services.AddCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -31,6 +33,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyOrigin());
 
 app.UseHttpsRedirection();
 
